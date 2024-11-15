028016b515efec40333469751b8e283dd4a1f763
DRaczek <damian.raczek4206695@gmail.com>
2024-11-15 01:33:30 +0100

    Class documentation created

--- konsola_styczen_2024/Biblioteka.cs
+++ konsola_styczen_2024/Biblioteka.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace konsola_styczen_2024
 {
 
+    /************************************************
+    klasa:  Program
+    opis:   Klasa zawiera metody narzędziowe do operacji na łańcuchach znaków, w tym liczenia samogłosk oraz usuwania powtórzeń znaków obok siebie. 
+    metody: Licz - zwraca liczbę samogłosek w podanym łańcuchu.
+            UsunPowtorzenia - zwraca łańcuch po usunięciu powtarzających się znaków obok siebie.
+    autor:  Damian Raczek
+    ************************************************/
     public static class Biblioteka
     {
         public static int Licz(string text)
