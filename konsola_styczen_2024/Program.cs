9f7c9cc3eaeff37cfa21113e44e0a461a89e7c93
DRaczek <damian.raczek4206695@gmail.com>
2024-11-15 01:33:54 +0100

    Test in main function implemented

--- konsola_styczen_2024/Program.cs
+++ konsola_styczen_2024/Program.cs
@@ -7,7 +7,19 @@ namespace konsola_styczen_2024
         public static void Main(string[] args)
         {
 
-            
+            Console.WriteLine("Witaj w programie narzędziowym do pracy z łańcuchami znaków!");
+
+            Console.Write("Podaj łańcuch znaków: ");
+            string tekst = Console.ReadLine() ?? "";
+
+            int liczbaSamoglosek = Biblioteka.Licz(tekst);
+            Console.WriteLine($"Liczba samogłosk w łańcuchu: {liczbaSamoglosek}");
+
+            string tekstBezPowtorzen = Biblioteka.UsunPowtorzenia(tekst);
+            Console.WriteLine($"Łańcuch po usunięciu powtórzeń: {tekstBezPowtorzen}");
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć...");
+            Console.ReadKey();
         }
     }
 }
