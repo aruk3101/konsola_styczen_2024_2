f48bab5a8e0a26b9b9e2fad1359573c9d9239080
DRaczek <damian.raczek4206695@gmail.com>
2024-11-15 01:33:17 +0100

    Method for deleting duplicates created and methods moved to other class

---
+++ konsola_styczen_2024/Biblioteka.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsola_styczen_2024
+{
+
+    public static class Biblioteka
+    {
+        public static int Licz(string text)
+        {
+            if (text == null || String.IsNullOrEmpty(text) == true) return 0;
+
+            string samogloski = "aąeęiouóyAĄEĘIOUÓY";
+            int i = 0;
+
+            foreach (char znak in text.ToArray())
+            {
+                if (samogloski.Contains(znak))
+                {
+                    i++;
+                }
+            }
+            return i;
+
+        }
+
+        public static string UsunPowtorzenia(string tekst)
+        {
+            if (string.IsNullOrEmpty(tekst))
+            {
+                return string.Empty;
+            }
+            return new string(tekst.ToArray()
+                .GroupBy(znak => znak)
+                .Select(grupa => grupa.Key)
+                .ToArray());
+        }
+    }
+}
--- konsola_styczen_2024/Program.cs
+++ konsola_styczen_2024/Program.cs
@@ -7,23 +7,8 @@ namespace konsola_styczen_2024
         public static void Main(string[] args)
         {
 
-        }
-
-        public static int Licz(string text)
-        {
-            if (text == null || String.IsNullOrEmpty(text) == true) return 0;
-
-            string samogloski = "aąeęiouóyAĄEĘIOUÓY";
-            int i = 0;
-
-            foreach (char znak in text.ToArray())
-            {
-                if (samogloski.Contains(znak))){
-                    i++;
-                }
-            }
-            return i;
-
+            
         }
     }
 }
+
